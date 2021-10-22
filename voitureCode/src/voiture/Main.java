ac9930b99f5620a9ca852cb869527fe94d453924
montnahp <thomas.vantomme01@gmail.com>
2021-10-22 21:57:32 +0200

    First Commit

---
+++ voitureCode/src/voiture/Main.java
@@ -0,0 +1,8 @@
+package voiture;
+
+public class Main {
+    public static void main(String[] args) {
+        Voiture voiture = new Voiture("Benz", 150, "jaune");
+        System.out.println(voiture);
+    }
+}
---
+++ voitureCode/src/voiture/Voiture.java
@@ -0,0 +1,53 @@
+package voiture;
+
+public class Voiture {
+    private String marque;
+    private String couleur;
+    private int nbChevaux;
+
+    //méthode de classe
+    private static final int NB_ROUES = 4;
+
+    public static int getNbRoues() {
+        return NB_ROUES;
+    }
+    Voiture(String marque){
+        this.marque = marque;
+    }
+
+    Voiture(String marque, int nbChevaux){
+        this.marque = marque;
+        this.nbChevaux = nbChevaux;
+        this.couleur = "";
+    }
+
+    Voiture(String marque, int nbChevaux, String couleur){
+        this.marque = marque;
+        this.nbChevaux = nbChevaux;
+        this.couleur = couleur;
+    }
+
+    public void augmenterNbChevaux(int nbChevaux){
+        if (nbChevaux > 0)
+            this.nbChevaux += nbChevaux;
+    }
+
+    public boolean equals(Voiture autreVoiture){
+        return marque.equals(autreVoiture.marque)
+                && nbChevaux == autreVoiture.nbChevaux
+                && couleur.equals(autreVoiture.couleur);
+    }
+
+    @Override
+    public String toString() {
+        return "Voiture{" +
+                "marque='" + marque + '\'' +
+                ", nbChevaux=" + nbChevaux +
+                ", couleur='" + couleur + '\'' +
+                '}';
+    }
+
+    public Voiture clone() {
+        return new Voiture(marque, nbChevaux, couleur);
+    }
+}
